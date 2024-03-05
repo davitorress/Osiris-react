e47ce96ef6f4ba560a8a70e33d7cf5a5a8587230
davitorress <dev.davitorres@gmail.com>
2024-03-05 20:33:55 -0300

    feat: add ionic icons to app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome"
+import { Ionicons } from "@expo/vector-icons"
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native"
 import { useFonts } from "expo-font"
 import { Stack } from "expo-router"
@@ -32,7 +32,7 @@ SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    ...FontAwesome.font,
+    ...Ionicons.font,
   })
 
   const [loadedUbuntu, errorUbuntu] = useUbuntuFonts({
