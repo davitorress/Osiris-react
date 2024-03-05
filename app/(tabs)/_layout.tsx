19b96da89d4c0ff97440f2ca75be70dd2d7206e2
davitorress <dev.davitorres@gmail.com>
2024-03-05 08:43:55 -0300

    feat: create utils folder

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import { Link, Tabs } from "expo-router"
 import { Pressable } from "react-native"
 
 import Colors from "@/constants/Colors"
-import { useClientOnlyValue } from "@/components/useClientOnlyValue"
+import { useClientOnlyValue } from "@/utils/useClientOnlyValue"
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
