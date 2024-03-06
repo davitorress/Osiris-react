fc43c0691a636ccc1d66dd278a3f2db03f1178e8
davitorress <dev.davitorres@gmail.com>
2024-03-06 15:54:29 -0300

    feat: add LoginForm component

--- components/forms/LoginForm.tsx
+++ components/forms/LoginForm.tsx
@@ -3,34 +3,104 @@ import { TextInput, View } from "react-native"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Controller, useForm } from "react-hook-form"
 
-const schema = z.object({
+import TextThemed from "@/components/TextThemed"
+import ButtonThemed from "@/components/ButtonThemed"
+
+const loginSchema = z.object({
   email: z.string().min(1, "O email é obrigatório!").email("Insira um email válido!"),
+  password: z
+    .string()
+    .min(1, "A senha é obrigatória!")
+    .min(3, "A senha deve ter no mínimo 3 caracteres!"),
 })
 
+type LoginData = z.infer<typeof loginSchema>
+
 export default function LoginForm() {
-  const { control, register } = useForm({
-    resolver: zodResolver(schema),
+  const {
+    control,
+    register,
+    formState: { errors },
+  } = useForm<LoginData>({
+    resolver: zodResolver(loginSchema),
   })
 
   return (
-    <View className="w-full mt-8">
-      {/* TODO: Implements React Hook Form */}
-      {/* DOCS: https://react-hook-form.com/get-started#ReactNative */}
-      <Controller
-        control={control}
-        rules={{
-          required: true,
-        }}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            value={value}
-            onBlur={onBlur}
-            onChangeText={onChange}
-            placeholder="email@email.com"
-          />
+    <View className="w-full">
+      <View className="w-full">
+        <TextThemed color="primary" size="h4" font="nunitoSemiBold" classes="mb-2">
+          E-mail:
+        </TextThemed>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              value={value}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              autoComplete="email"
+              placeholder="email@email.com"
+              className="w-full p-2 rounded-lg bg-gray-medium text-white placeholder:text-gray-dark"
+            />
+          )}
+          {...register("email")}
+        />
+        {errors.email && (
+          <TextThemed color="alert" size="body2" font="ubuntuRegular" classes="mt-1">
+            {errors.email.message}
+          </TextThemed>
         )}
-        {...register("email")}
-      />
+      </View>
+
+      <View className="w-full mt-5">
+        <TextThemed color="primary" size="h4" font="nunitoSemiBold" classes="mb-2">
+          Senha:
+        </TextThemed>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              value={value}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              secureTextEntry
+              placeholder="********"
+              autoComplete="password"
+              className="w-full p-2 rounded-lg bg-gray-medium text-white placeholder:text-gray-dark"
+            />
+          )}
+          {...register("email")}
+        />
+        {errors.email && (
+          <TextThemed color="alert" size="body2" font="ubuntuRegular" classes="mt-1">
+            {errors.email.message}
+          </TextThemed>
+        )}
+      </View>
+
+      <View className="w-full mt-5">
+        <ButtonThemed type="button" color="primary" size="full" shape="rounded">
+          <TextThemed color="white" size="h4" font="nunitoBold">
+            Entrar
+          </TextThemed>
+        </ButtonThemed>
+      </View>
+
+      <View className="w-full mt-3">
+        <TextThemed
+          type="link"
+          url="/register"
+          color="primary"
+          size="h4"
+          font="nunitoRegular"
+          classes="mt-2"
+        >
+          Não possui uma conta?{" "}
+          <TextThemed color="primary" size="h4" font="nunitoBold" classes="underline">
+            Crie aqui
+          </TextThemed>
+        </TextThemed>
+      </View>
     </View>
   )
 }
