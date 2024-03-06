19459e3c9ad7e28222afae13f6ed019d77d4da5c
davitorress <dev.davitorres@gmail.com>
2024-03-06 20:58:25 -0300

    style: LoginPage alignment and spacing

--- app/login.tsx
+++ app/login.tsx
@@ -6,14 +6,14 @@ import { View } from "react-native"
 
 export default function LoginScreen() {
   return (
-    <SafeAreaView className="m-0 pt-28 px-6 flex-1 bg-white">
+    <SafeAreaView className="m-0 pt-28 px-6 flex-1 items-center bg-white">
       <ImageWithPlaceholder
         alt="Osiris"
         className="w-20 h-20"
         source={require("../assets/images/osiris_logo.png")}
       />
 
-      <View className="w-full mt-8 px-6">
+      <View className="w-full mt-12 px-4">
         <LoginForm />
       </View>
     </SafeAreaView>
