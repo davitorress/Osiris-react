44be63dd8176276a371d04794ae1445d20e5709c
davitorress <dev.davitorres@gmail.com>
2024-03-13 21:07:32 -0300

    style: add tabBar design

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,22 @@
 import React from "react"
 import { Tabs } from "expo-router"
+import { StyleSheet } from "react-native"
 
+import Colors from "@/constants/Colors"
 import IonIcon from "@/components/basic/IonIcon"
+
+const styles = StyleSheet.create({
+  tabBar: {
+    borderTopWidth: 1.5,
+    borderLeftWidth: 1.5,
+    borderRightWidth: 1.5,
+    borderTopLeftRadius: 18,
+    borderTopRightRadius: 18,
+    borderTopColor: Colors.light.green.dark,
+    borderLeftColor: Colors.light.green.dark,
+    borderRightColor: Colors.light.green.dark,
+  },
+})
 
 export default function TabLayout() {
   return (
@@ -14,6 +29,7 @@ export default function TabLayout() {
         name="index"
         options={{
           tabBarShowLabel: false,
+          tabBarStyle: styles.tabBar,
           tabBarIcon: ({ focused }) => (
             <IonIcon name={focused ? "home" : "home-outline"} size="large" color="tertiary" />
           ),
