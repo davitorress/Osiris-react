1f36168ce38df83439b76c419ce693c0cb894eac
mfelipegs <matheusg.dasilva7@gmail.com>
2024-03-19 20:23:02 -0300

    test: value test consts

--- __tests__/forms/register.test.tsx
+++ __tests__/forms/register.test.tsx
@@ -6,30 +6,34 @@ test("submitting form with valid data should call onSubmit", async () => {
   const onSubmit = jest.fn()
   const { getByPlaceholderText } = render(<RegisterForm onSubmit={onSubmit} />)
 
+  const nameValueTest = "John Doe"
+  const emailValueTest = "john.doe@example.com"
+  const passwordValueTest = "password123"
+
   const nameInput = getByPlaceholderText("Digite o seu nome completo")
   const emailInput = getByPlaceholderText("Digite o seu e-mail")
   const passwordInput = getByPlaceholderText("Digite a sua senha")
   const confirmPasswordInput = getByPlaceholderText("Digite novamente a sua senha")
 
-  fireEvent.changeText(nameInput, "John Doe")
-  fireEvent.changeText(emailInput, "john.doe@example.com")
-  fireEvent.changeText(passwordInput, "password123")
-  fireEvent.changeText(confirmPasswordInput, "password123")
+  fireEvent.changeText(nameInput, nameValueTest)
+  fireEvent.changeText(emailInput, emailValueTest)
+  fireEvent.changeText(passwordInput, passwordValueTest)
+  fireEvent.changeText(confirmPasswordInput, passwordValueTest)
 
   await act(async () => {
     await onSubmit({
-      name: "John Doe",
-      email: "john.doe@example.com",
-      password: "password123",
-      confirmPassword: "password123",
+      name: nameValueTest,
+      email: emailValueTest,
+      password: passwordValueTest,
+      confirmPassword: passwordValueTest,
     })
   })
 
   expect(onSubmit).toHaveBeenCalledTimes(1)
   expect(onSubmit).toHaveBeenCalledWith({
-    name: "John Doe",
-    email: "john.doe@example.com",
-    password: "password123",
-    confirmPassword: "password123",
+    name: nameValueTest,
+    email: emailValueTest,
+    password: passwordValueTest,
+    confirmPassword: passwordValueTest,
   })
 })
