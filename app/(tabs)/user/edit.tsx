46fbcda3cd00035f97e4dd5f270da6473d75b20f
davitorress <dev.davitorres@gmail.com>
2024-04-21 09:08:41 -0300

    feat: edit user page

--- app/(tabs)/user/edit.tsx
+++ app/(tabs)/user/edit.tsx
@@ -1,5 +1,55 @@
-import { View } from "react-native"
+import { useRouter } from "expo-router"
+import { ScrollView, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+import IonIcon from "@/components/basic/IonIcon"
+import EditUserForm from "@/components/forms/EditUserForm"
+import ButtonThemed from "@/components/themed/ButtonThemed"
+import ImageWithPlaceholder from "@/components/basic/ImageWithPlaceholder"
 
 export default function EditUserScreen() {
-  return <View>Edit User</View>
+  const router = useRouter()
+
+  const image =
+    "http://res.cloudinary.com/dvxkj7fwq/image/upload/v1699569417/34f93c4d-fc34-4a10-b908-82d67d61d358.png"
+
+  return (
+    <SafeAreaView className="m-o flex-1">
+      <ScrollView>
+        <View className="pt-6 px-6">
+          <View className="w-full">
+            <View className="w-fit flex-row absolute top-0 left-0">
+              <ButtonThemed type="icon" shape="circle" onClick={() => router.push("/(tabs)/user")}>
+                <IonIcon name="return-down-back" size="huge" color="white" />
+              </ButtonThemed>
+            </View>
+          </View>
+
+          <View className="relative self-center w-fit">
+            <View className="items-center justify-center w-40 h-40 rounded-full bg-gray-light overflow-hidden">
+              {image ? (
+                <ImageWithPlaceholder
+                  alt="nome"
+                  source={image}
+                  className="w-40 h-40 rounded-full"
+                />
+              ) : (
+                <IonIcon name="person-outline" color="primary" size="massive" />
+              )}
+            </View>
+
+            <View className="absolute bottom-0 right-0">
+              <ButtonThemed shape="circle" onClick={undefined}>
+                <IonIcon name="pencil-outline" color="white" size="veryHuge" />
+              </ButtonThemed>
+            </View>
+          </View>
+
+          <View className="w-full mt-8">
+            <EditUserForm onSubmit={() => {}} />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
---
+++ components/forms/EditUserForm.tsx
@@ -0,0 +1,136 @@
+import { z } from "zod"
+import { View } from "react-native"
+import { useRouter } from "expo-router"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+
+import Sizes from "@/constants/Sizes"
+import Input from "@/components/basic/Input"
+import TextThemed from "@/components/themed/TextThemed"
+import ButtonThemed from "@/components/themed/ButtonThemed"
+
+const editUserSchema = z.object({
+  name: z
+    .string({ required_error: "O nome é obrigatório!" })
+    .min(1, "O nome é obrigatório!")
+    .transform((name) => name.trim()),
+  email: z
+    .string({ required_error: "O email é obrigatório!" })
+    .min(1, "O email é obrigatório!")
+    .email("Insira um email válido!"),
+  newPassword: z
+    .string()
+    .min(3, "A senha deve ter no mínimo 3 caracteres!")
+    .transform((password) => password.trim())
+    .optional(),
+})
+
+type EditUserData = z.infer<typeof editUserSchema>
+
+interface EditUserFormProps {
+  onSubmit: (data: EditUserData) => void
+}
+
+export default function EditUserForm({ onSubmit }: EditUserFormProps) {
+  const router = useRouter()
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EditUserData>({
+    resolver: zodResolver(editUserSchema),
+  })
+
+  return (
+    <View className="w-full" style={{ gap: Sizes.veryHuge }}>
+      <View className="w-full">
+        <TextThemed color="primary" size="h4" font="nunitoSemiBold" classes="mb-2">
+          Nome completo:
+        </TextThemed>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              value={value}
+              onBlur={onBlur}
+              onChange={onChange}
+              font="nunitoSemiBold"
+              placeholder="Digite o seu nome completo"
+            />
+          )}
+          {...register("name")}
+        />
+        {errors.name && (
+          <TextThemed color="alert" size="body2" font="ubuntuRegular" classes="mt-1">
+            {errors.name.message}
+          </TextThemed>
+        )}
+      </View>
+
+      <View className="w-full">
+        <TextThemed color="primary" size="h4" font="nunitoSemiBold" classes="mb-2">
+          Email:
+        </TextThemed>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              value={value}
+              onBlur={onBlur}
+              onChange={onChange}
+              font="nunitoSemiBold"
+              placeholder="Digite o seu email"
+            />
+          )}
+          {...register("email")}
+        />
+        {errors.email && (
+          <TextThemed color="alert" size="body2" font="ubuntuRegular" classes="mt-1">
+            {errors.email.message}
+          </TextThemed>
+        )}
+      </View>
+
+      <View className="w-full">
+        <TextThemed color="primary" size="h4" font="nunitoSemiBold" classes="mb-2">
+          Nova senha:
+        </TextThemed>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              onBlur={onBlur}
+              value={value ?? ""}
+              onChange={onChange}
+              font="nunitoSemiBold"
+              placeholder="Digite caso deseje alterar"
+              secureTextEntry
+            />
+          )}
+          {...register("newPassword")}
+        />
+        {errors.newPassword && (
+          <TextThemed color="alert" size="body2" font="ubuntuRegular" classes="mt-1">
+            {errors.newPassword.message}
+          </TextThemed>
+        )}
+      </View>
+
+      <View className="w-full flex-row justify-between">
+        <ButtonThemed color="alert" onClick={() => router.push("/(tabs)/user")} classes="w-[45%]">
+          <TextThemed color="white" size="h4" font="nunitoBold">
+            Cancelar
+          </TextThemed>
+        </ButtonThemed>
+
+        <ButtonThemed color="secondary" onClick={handleSubmit(onSubmit)} classes="w-[45%]">
+          <TextThemed color="white" size="h4" font="nunitoBold">
+            Salvar
+          </TextThemed>
+        </ButtonThemed>
+      </View>
+    </View>
+  )
+}
