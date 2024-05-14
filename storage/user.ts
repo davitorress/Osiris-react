932ce141fcb4ddbb6c261ca44abfcf486139bc17
davitorress <dev.davitorres@gmail.com>
2024-05-13 21:41:12 -0300

    feat: add user store

---
+++ storage/user.ts
@@ -0,0 +1,45 @@
+import { create } from "zustand"
+
+import { User } from "@/modules/user/types"
+
+interface UserStoreProps {
+  id?: string
+  user?: User
+  token?: string
+}
+
+interface UserActions {
+  setId: (id?: string) => void
+  setUser: (user?: User) => void
+  setToken: (token?: string) => void
+}
+
+type UserStore = UserStoreProps & {
+  actions: UserActions
+}
+
+const initialState: UserStoreProps = {
+  id: undefined,
+  user: undefined,
+  token: undefined,
+}
+
+const userStore = create<UserStore>((set, get) => ({
+  ...initialState,
+  actions: {
+    setId: (id) => {
+      if (id === get().id) return
+      set({ id })
+    },
+    setToken: (token) => {
+      if (token === get().token) return
+      set({ token })
+    },
+    setUser: (user) => {
+      if (user === get().user) return
+      set({ user })
+    },
+  },
+}))
+
+export default userStore
