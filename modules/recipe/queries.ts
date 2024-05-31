e2113292e0cd7081046f22cc4e7523f3cfe8a322
davitorress <dev.davitorres@gmail.com>
2024-05-31 10:36:52 -0300

    fix: invalidate recipe page after edit recipe image

--- modules/recipe/queries.ts
+++ modules/recipe/queries.ts
@@ -211,8 +211,9 @@ export const useUpdateRecipeImage = () => {
 
   return useMutation({
     mutationFn: mutation,
-    onSuccess: () => {
+    onSuccess: (data) => {
       void queryClient.invalidateQueries({ queryKey: ["recipes"] })
+      void queryClient.invalidateQueries({ queryKey: ["recipe", data.id] })
     },
     onError: (error) => {
       console.error(error)
